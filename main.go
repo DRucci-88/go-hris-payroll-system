a3be29d09f0e9690072bec561af8ab9b39afa9a2
rucci <d.rucci.2001@gmail.com>
2026-06-15 18:41:16 +0700

    Spesifikasi Teknis

--- main.go
+++ main.go
@@ -1,11 +1,204 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
 /*
 day5-abstract-struktur-data-dinamis.pdf
 Sesi 6: Soal Ujian Akhir Modul (Studi Kasus HRIS)
 Studi Kasus: Sistem Manajemen Kepegawaian & Payroll Terintegrasi
 */
 
-func main() {
+var (
+	ErrNotNegativeValue    = errors.New("value tidak boleh negatif")
+	ErrEmptyEmployeeName   = errors.New("nama karyawan tidak boleh kosong")
+	ErrDuplicateEmployeeID = errors.New("karyawan dengan ID tersebut sudah terdaftar")
+	ErrPercentageInvalid   = errors.New("tidak bisa dibawah 0.0 dan diatas 1.0")
+)
 
+type EmployeeTypeEnum string
+
+const (
+	FULLTIME  EmployeeTypeEnum = "FullTime"
+	CONTRACT  EmployeeTypeEnum = "Contract"
+	FREELANCE EmployeeTypeEnum = "Freelance"
+)
+
+type PayrollCalculator interface {
+	CalculateSalary() (float64, error)
+	GetEmployeeType() EmployeeTypeEnum
+}
+
+type FullTimeEmployee struct {
+	BaseSalary float64
+	Allowance  float64
+	TaxRate    float64
+}
+
+func NewFullTimeEmployee(
+	baseSalary float64,
+	allowance float64,
+	taxRate float64,
+) (*FullTimeEmployee, error) {
+	if baseSalary < 0.0 || allowance < 0.0 {
+		return nil, fmt.Errorf("MonthlyRate dan PerformanceBonus [%w]", ErrNotNegativeValue)
+	}
+	if taxRate < 0.0 || taxRate > 1.0 {
+		return nil, fmt.Errorf("TaxRate %w", ErrPercentageInvalid)
+	}
+	return &FullTimeEmployee{
+		BaseSalary: baseSalary,
+		Allowance:  allowance,
+		TaxRate:    taxRate,
+	}, nil
+}
+func (emp *FullTimeEmployee) GetEmployeeType() EmployeeTypeEnum {
+	return FULLTIME
+}
+func (emp *FullTimeEmployee) CalculateSalary() (float64, error) {
+	return (emp.BaseSalary + emp.Allowance) * (1.0 - emp.TaxRate), nil
+	// return 0.0, nil
+}
+
+type ContractEmployee struct {
+	MonthlyRate      float64
+	PerformanceBonus float64
+}
+
+func NewContractEmployee(
+	monthlyRate float64,
+	performanceBonus float64,
+) (*ContractEmployee, error) {
+	if monthlyRate < 0.0 || performanceBonus < 0.0 {
+		return nil, fmt.Errorf("MonthlyRate dan PerformanceBonus [%w]", ErrNotNegativeValue)
+	}
+	return &ContractEmployee{
+		MonthlyRate:      monthlyRate,
+		PerformanceBonus: performanceBonus,
+	}, nil
+}
+func (emp *ContractEmployee) GetEmployeeType() EmployeeTypeEnum {
+	return CONTRACT
+}
+func (emp *ContractEmployee) CalculateSalary() (float64, error) {
+	return emp.MonthlyRate + emp.PerformanceBonus, nil
+	// return 0.0, nil
+}
+
+type Freelancer struct {
+	HourylyLate float64
+	HoursWorked int
+}
+
+func NewFreelancer(
+	hourlyRate float64,
+	hoursWorked int,
+) (*Freelancer, error) {
+	if hourlyRate < 0.0 || hoursWorked < 0 {
+		return nil, fmt.Errorf("HourylyLate dan HourylyLate [%w]", ErrNotNegativeValue)
+	}
+	return &Freelancer{
+		HourylyLate: hourlyRate,
+		HoursWorked: hoursWorked,
+	}, nil
+}
+func (emp *Freelancer) GetEmployeeType() EmployeeTypeEnum {
+	return FREELANCE
+}
+func (emp *Freelancer) CalculateSalary() (float64, error) {
+	return emp.HourylyLate * float64(emp.HoursWorked), nil
+	// return 0.0, nil
+}
+
+type HRIS struct {
+	Employees map[string]string            // EmployeeID -> Nama Karyawan
+	Payrolls  map[string]PayrollCalculator // EmployeeID -> PayrollCalculator
+}
+
+func (hris *HRIS) RegisterEmployee(
+	id string, // EmployeeID
+	name string,
+	payroll PayrollCalculator,
+) error {
+	if len(name) <= 0 {
+		return ErrEmptyEmployeeName
+	}
+	_, existEmployee := hris.Employees[id]
+	_, existPayroll := hris.Payrolls[id]
+	if existEmployee == existPayroll { // Jika ID tersebut sudah terdaftar di 2 map tersebut
+		return fmt.Errorf("EmployeeID [%s] %w", id, ErrDuplicateEmployeeID)
+	}
+	hris.Employees[id] = name
+	hris.Payrolls[id] = payroll
+	return nil
+}
+
+func (hris *HRIS) CalculateTotalPayout() float64 {
+	if len(hris.Employees) != len(hris.Payrolls) {
+		fmt.Println("WADUH Jumlah map tidak sama")
+		fmt.Println(hris.Employees)
+		fmt.Println(hris.Payrolls)
+		return 0.0
+	}
+
+	var totalSalary float64
+	for _, payroll := range hris.Payrolls {
+		if salary, err := payroll.CalculateSalary(); err == nil {
+			totalSalary += salary
+		}
+
+	}
+	return totalSalary
+}
+
+func (hris *HRIS) PrintPayrollReport() {
+	fmt.Println("\n========== PAYROLL REPORT ==========")
+
+	for employeeID, payroll := range hris.Payrolls {
+		name := hris.Employees[employeeID]
+
+		salary, err := payroll.CalculateSalary()
+		if err != nil {
+			fmt.Printf(
+				"[ERROR] Gagal menghitung gaji karyawan %s: %v\n",
+				name,
+				err,
+			)
+			continue
+		}
+
+		fmt.Println("------------------------------------")
+		fmt.Printf("Employee ID   : %s\n", employeeID)
+		fmt.Printf("Nama          : %s\n", name)
+		fmt.Printf("Tipe          : %s\n", payroll.GetEmployeeType())
+
+		// Type Switch
+		switch emp := payroll.(type) {
+
+		case *FullTimeEmployee:
+			fmt.Printf("Base Salary   : Rp %.2f\n", emp.BaseSalary)
+			fmt.Printf("Allowance     : Rp %.2f\n", emp.Allowance)
+			fmt.Printf("Tax Rate      : %.2f%%\n", emp.TaxRate*100)
+
+		case *ContractEmployee:
+			fmt.Printf("Monthly Rate  : Rp %.2f\n", emp.MonthlyRate)
+			fmt.Printf("Bonus         : Rp %.2f\n", emp.PerformanceBonus)
+
+		case *Freelancer:
+			fmt.Printf("Hourly Rate   : Rp %.2f\n", emp.HourylyLate)
+			fmt.Printf("Hours Worked  : %d jam\n", emp.HoursWorked)
+
+		default:
+			fmt.Println("Unknown Employee Type")
+		}
+
+		fmt.Printf("Total Salary  : Rp %.2f\n", salary)
+	}
+
+	fmt.Println("====================================")
+}
+
+func main() {
 }
